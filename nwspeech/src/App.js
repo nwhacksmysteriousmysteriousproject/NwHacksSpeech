ac2aec164b476bf86119fc21b9096aeac05409a3
Amy Mao <amy_m123@icloud.com>
2019-01-26 17:19:25 -0800

    setup

--- nwspeech/src/App.js
+++ nwspeech/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import azure from './component/azure.js';
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <azure></azure>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
---
+++ nwspeech/src/component/azure.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+class Azure extends React.Component {
+  render(){
+    return (<div></div>);
+  }
+}
